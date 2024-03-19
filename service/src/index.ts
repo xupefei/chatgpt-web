f5ac8d0dd7a9c7b6bab17d479490aa127c5f3db4
Paddy Xu <xupaddy@gmail.com>
2024-03-19 11:01:56 +0100

    fix cloudflared streaming

--- service/src/index.ts
+++ service/src/index.ts
@@ -20,7 +20,7 @@ app.all('*', (_, res, next) => {
 })
 
 router.post('/chat-process', [auth, limiter], async (req, res) => {
-  res.setHeader('Content-type', 'application/octet-stream')
+  res.setHeader('Content-type', 'text/event-stream')
 
   try {
     const { prompt, options = {}, systemMessage, temperature, top_p } = req.body as RequestProps
